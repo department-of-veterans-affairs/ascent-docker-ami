0d286a503378c30c589de2f841642e0f1e2ac73f
stevenwhayes <shayes@integratedsecure.com>
2017-08-14 16:04:32 -0500

    Swarm joining script

--- swarm.sh
+++ swarm.sh
@@ -2,10 +2,14 @@
 
 
 [[ -z "$DOCKER_SWARM_TOKEN" ]] && echo "Initializing swarm" && sudo docker swarm init | sudo tee  /etc/docker/swarm-worker-token \
-&& sudo docker swarm join-token manager | sudo tee /etc/docker/swarm-manager-token
+&& sudo docker swarm join-token manager | sudo tee /etc/docker/swarm-manager-token && exit 0
 
 
+# Set swarm join at next restart so instance is in security group that is able to reach existing master
+[[ -n "$DOCKER_SWARM_TOKEN" ]] && [[ -n "$DOCKER_SWARM_MASTER" ]] && \
+sudo sed -i '/ExecStart=\/usr\/bin\/dockerd/ a ExecStartPost=-/usr/bin/docker swarm join --token '${DOCKER_SWARM_TOKEN}' '${DOCKER_SWARM_MASTER}'' \
+/usr/lib/systemd/system/docker.service && sudo systemctl daemon-reload && exit 0
 
-#Discover listening manager
-#sudo yum -y install netcat
+#sudo docker swarm join  --token ${DOCKER_SWARM_TOKEN} ${DOCKER_SWARM_MASTER} && exit 0
 
+exit 1
