dd442d6c82275844e270300a0f414787cf2c1d3f
jasonluck <jluck@meetveracity.com>
2017-11-20 21:30:07 -0500

    get instance ip to bind consul agent to

--- consul-agent/startconsul.sh
+++ consul-agent/startconsul.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 SG=`curl -s http://169.254.169.254/latest/meta-data/security-groups | tr '\n' '\-'`
+IP=`curl -s http://169.254.169.254/latest/meta-data/local-ipv4`
 CONFIG_FILE=`ls /app/consul/config/swarm-*`
 
 grep -q XX_SG $CONFIG_FILE && sed -i "s,XX_SG,$SG," $CONFIG_FILE
 
-/usr/bin/consul agent -bind `hostname -i` -config-dir /app/consul/config
+/usr/bin/consul agent -bind $IP -config-dir /app/consul/config
 
