1bb5aed262e99025603aba4c3cc04ee082164a9a
jasonluck <jluck@meetveracity.com>
2018-02-05 15:06:14 -0500

    removing redundant install of aws-cli and jq

--- install-vault-ca.sh
+++ install-vault-ca.sh
@@ -6,16 +6,6 @@ set -x
 sudo yum -y install wget unzip deltarpm nmap curl bind-utils ntp
 sudo systemctl enable ntpd
 
-# aws cli
-curl -s "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
-unzip awscli-bundle.zip
-sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
-rm -rf awscli-bundle awscli-bundle.zip
-
-# jq command line JSON parser
-sudo wget -O /usr/bin/jq https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64
-sudo chmod 755 /usr/bin/jq
-
 # Install our Vault CA service
 sudo chown root:root /tmp/vault-ca.sh /tmp/vault-ca.service
 sudo mv /tmp/vault-ca.sh /root/vault-ca.sh
