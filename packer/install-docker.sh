88b3528efef0ae06a3d340790ff9889adacead58
Elisabeth Sorrell <sorrelle04@gmail.com>
2018-08-17 10:56:31 -0500

    add install of consul-template to the install-docker script"

--- packer/install-docker.sh
+++ packer/install-docker.sh
@@ -12,6 +12,13 @@ sudo yum -y remove docker \
 #basic packages and prereq
 sudo yum -y install wget unzip deltarpm curl
 
+#install curl for getting certificates
+curl -s -L -o consul-template_0.19.0_linux_amd64.tgz https://releases.hashicorp.com/consul-template/0.19.0/consul-template_0.19.0_linux_amd64.tgz; \
+    sudo tar -xzf consul-template_0.19.0_linux_amd64.tgz; \
+    sudo mv consul-template /usr/local/bin/consul-template; \
+    sudo chmod +x /usr/local/bin/consul-template; \
+    sudo rm -f consul-template_0.19.0_linux_amd64.tgz
+
 sudo groupadd docker
 sudo yum-config-manager --enable rhui-REGION-rhel-server-extras
 sudo yum -y install docker-latest
